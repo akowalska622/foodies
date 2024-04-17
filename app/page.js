a1eb48aa445a5ae9f8ab3c5ddad9e38494196acf
Ania Kowalska <a.kowalska622@gmail.com>
2024-04-17 15:39:09 +0200

    style: add whitespace in "Next Level"

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
         <div className={classes.slideshow}></div>
         <div>
           <div className={classes.hero}>
-            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <h1>Next Level Food for Next Level Foodies</h1>
             <p>Taste & share food from all over the world.</p>
           </div>
           <div className={classes.cta}>
@@ -22,25 +22,25 @@ export default function Home() {
         <section className={classes.section}>
           <h2>How it works</h2>
           <p>
-            NextLevel Food is a platform for foodies to share their favorite
+            Next Level Food is a platform for foodies to share their favorite
             recipes with the world. It&apos;s a place to discover new dishes,
             and to connect with other food lovers.
           </p>
           <p>
-            NextLevel Food is a place to discover new dishes, and to connect
+            Next Level Food is a place to discover new dishes, and to connect
             with other food lovers.
           </p>
         </section>
 
         <section className={classes.section}>
-          <h2>Why NextLevel Food?</h2>
+          <h2>Why Next Level Food?</h2>
           <p>
-            NextLevel Food is a platform for foodies to share their favorite
+            Next Level Food is a platform for foodies to share their favorite
             recipes with the world. It&apos;s a place to discover new dishes,
             and to connect with other food lovers.
           </p>
           <p>
-            NextLevel Food is a place to discover new dishes, and to connect
+            Next Level Food is a place to discover new dishes, and to connect
             with other food lovers.
           </p>
         </section>
