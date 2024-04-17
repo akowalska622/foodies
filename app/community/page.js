c08217aac6adc1a549d029e864d86c18fd198577
Ania Kowalska <a.kowalska622@gmail.com>
2024-04-17 15:22:30 +0200

    feat: add basic routing

---
+++ app/community/page.js
@@ -0,0 +1,9 @@
+export default function CommunityPage() {
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        This is community page
+      </h1>
+    </main>
+  );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+export default function ShareMealPage() {
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        This is meals share page
+      </h1>
+    </main>
+  );
+}
